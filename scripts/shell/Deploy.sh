1dac3fc24db44ce78aaa671b56be6d9bcf5a3a68
g-brodiei <chunjuichen@gmail.com>
2023-03-06 23:12:49 +0800

    Init automatic deployment

---
+++ scripts/shell/Deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$PROD_ENVIRONMENT" = true ]; then
+  echo "Ready to deploy, this is production site"
+  if command -v drush >/dev/null 2>&1; then
+    echo "Drush is installed, running deploy script..."
+    drush deploy
+  else
+    echo "Drush is not installed, please install it and try again."
+  fi
+else
+  echo "Not a production environment, skipping deployment."
+fi
