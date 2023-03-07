4f293e82e877339ebce5db7a3da8187f64bb6fc3
g-brodiei <chunjuichen@gmail.com>
2023-03-07 10:19:26 +0800

    add change directory

--- scripts/shell/Deploy.sh
+++ scripts/shell/Deploy.sh
@@ -2,6 +2,7 @@
 
 if [ "$PROD_ENVIRONMENT" = true ]; then
   echo "Ready to deploy, this is production site"
+  cd /app
 
   # Wait for the database connection
   echo "Waiting for database connection..."
@@ -13,6 +14,7 @@ if [ "$PROD_ENVIRONMENT" = true ]; then
   # Run the deploy command
   if command -v drush >/dev/null 2>&1; then
     echo "Drush is installed, running deploy script..."
+    cd /app
     drush deploy -vvv -y
   else
     echo "Drush is not installed, please install it and try again."
