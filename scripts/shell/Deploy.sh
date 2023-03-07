c8fe33934afbf81f9f576f92f23a34f97e6fdaeb
g-brodiei <chunjuichen@gmail.com>
2023-03-07 10:04:33 +0800

    Update deploy command

--- scripts/shell/Deploy.sh
+++ scripts/shell/Deploy.sh
@@ -4,7 +4,7 @@ if [ "$PROD_ENVIRONMENT" = true ]; then
   echo "Ready to deploy, this is production site"
   if command -v drush >/dev/null 2>&1; then
     echo "Drush is installed, running deploy script..."
-    drush deploy
+    drush deploy -vvv -y
   else
     echo "Drush is not installed, please install it and try again."
   fi
