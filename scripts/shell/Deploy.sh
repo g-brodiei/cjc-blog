00e2bf21533acf05fe7f7215c195458b32ebb198
g-brodiei <chunjuichen@gmail.com>
2023-03-07 10:14:23 +0800

    Add await sql connection for deployment

--- scripts/shell/Deploy.sh
+++ scripts/shell/Deploy.sh
@@ -2,12 +2,22 @@
 
 if [ "$PROD_ENVIRONMENT" = true ]; then
   echo "Ready to deploy, this is production site"
+
+  # Wait for the database connection
+  echo "Waiting for database connection..."
+  until drush sql-connect > /dev/null 2>&1; do
+    sleep 1
+  done
+  echo "Database connection established."
+
+  # Run the deploy command
   if command -v drush >/dev/null 2>&1; then
     echo "Drush is installed, running deploy script..."
     drush deploy -vvv -y
   else
     echo "Drush is not installed, please install it and try again."
   fi
+
 else
   echo "Not a production environment, skipping deployment."
 fi
